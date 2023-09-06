8b048edcf223cc3880a0a0d321713b801acc3ba1
Reyes-J-R <reyesjoeroshan@gmail.com>
2023-09-06 23:04:22 +0530

    Maintanance Screen Created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Neithan Nigga</h1>
+        <h1 className='maintanance-title'>This site is under construction</h1>
+        <p className='maintanance-description'>This site is courtesy of Future eSports</p>
+        <div className='img-holder'>
+          <img className="fesports-logo" src="./media/2_20230824_062105_0001.png" />
+          <img className="fesports-credits" src="./media/credit.png" />
+        </div>
       </header>
     </div>
   );
