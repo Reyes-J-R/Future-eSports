41287bf09353eff0c34045bb2980e8aa4d1bdd78
Reyes-J-R <reyesjoeroshan@gmail.com>
2023-09-06 23:14:27 +0530

    added dev contact

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
       <header className="App-header">
         <h1 className='maintanance-title'>This site is under construction</h1>
         <p className='maintanance-description'>This site is courtesy of Future eSports</p>
-        <div className='img-holder'>
+        <div className='img-holder '>
           <img className="fesports-logo" src="./media/2_20230824_062105_0001.png" />
           <img className="fesports-credits" src="./media/credit.png" />
         </div>
+        <a href="wa.me/7510591162">Contact Dev</a>
       </header>
     </div>
   );
